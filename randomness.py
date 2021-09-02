c8956899686c8513ab7aee8cd27b57f7eaf97346
Jakub Olszewski <kubacki0088@gmail.com>
2021-09-02 12:45:58 +0200

    Expand throw_dice to include multiple dice

--- randomness.py
+++ randomness.py
@@ -1,11 +1,13 @@
 import random
 """Module with randomness used in the game"""
 
-def throw_dice(d: int):
-    return random.randint(1,d)
+def throw_dice(d: int, n: int):
+    """Throw a n-number of d-sided dice"""
+    return [random.randint(1,d) for i in range(n)]
 
 
 if __name__ == "__main__":
     while True:
         d = int(input("How many sides?: "))
-        print(throw_dice(d))
+        n = int(input("How many dice?: "))
+        print(throw_dice(d,n))
