c3ed0ca8461dc2d7ac83b23fce4b28d535058c23
Jakub Olszewski <kubacki0088@gmail.com>
2021-09-03 16:52:37 +0200

    Setup initial atributes

--- character.py
+++ character.py
@@ -1,8 +1,16 @@
 """Define the Character class with stats, equipment, stuff"""
 
 class Character:
-    pass
+    def __init__(self) -> None:
+        self.level = 1
+        self.name = ''
+        self.char_class = ''
 
+        # Strength, Dexterity, Constitution, Intelligence, Wisdom, Charisma
+        self.stats = [0,0,0,0,0,0]
+
+    def setup_character(self):
+        pass
 
 if __name__ == "__main__":
     a = Character()
