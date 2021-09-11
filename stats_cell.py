16cddfddfe970119f3d31618560b935925979d7e
Jakub Olszewski <kubacki0088@gmail.com>
2021-09-11 12:04:16 +0200

    Change font size and family

--- stats_cell.py
+++ stats_cell.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter.font import Font
 from tkinter.ttk import Spinbox
 
 class stats_cell(tk.Frame):
@@ -13,7 +14,8 @@ class stats_cell(tk.Frame):
         self.container_frame.pack(fill=tk.BOTH)
 
         self.stat_lab = tk.Label(master=self.container_frame, text=self.stat_name)
-        self.stat_val_box = Spinbox(master=self.container_frame, from_=0, to=99, textvariable=self.spin_val)
+        self.stat_val_box = Spinbox(master=self.container_frame, from_=0, to=99, textvariable=self.spin_val,
+                                    font=Font(family= 'Courier', size = 13), width=5, justify=tk.CENTER)
         self.stat_lab.pack(side=tk.LEFT)
         self.stat_val_box.pack(side=tk.RIGHT)
 
